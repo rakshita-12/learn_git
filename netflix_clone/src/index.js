594d46855ed9825eaeec9c06b16af5b59f1fd73a
rakshita <rakshudeekshu.12@gmail.com>
2025-12-07 11:17:14 +0530

    Prepare app for GitHub Pages

--- netflix_clone/src/index.js
+++ netflix_clone/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // <-- import here
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,10 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter basename="/netflix_clone">  {/* <-- wrap App */}
+
     <App />
+     </BrowserRouter>
   </React.StrictMode>
 );
 
