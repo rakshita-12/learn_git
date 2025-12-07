9813cd415384f083008494268b7b212d16f646fd
rakshita <rakshudeekshu.12@gmail.com>
2025-12-07 16:00:56 +0530

    Add homepage and deploy scripts, update CSS and components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,18 @@
-import React from "react";
-import "./Navbar.css";
-
-function Navbar() {
-  return (
-    <nav className="navbar">
-      <h2 className="logo">My Portfolio</h2>
-      <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#skills">Skills</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
-    </nav>
-  );
-}
-
+import "./Navbar.css"; 
+ 
+function Navbar() { 
+  return ( 
+    <nav className="navbar"> 
+      <h1 className="logo">Portfolio</h1> 
+      <ul className="nav-links"> 
+        <li>Home</li> 
+        <li>About</li> 
+        <li>Skills</li> 
+        <li>Projects</li> 
+        <li>Contact</li> 
+      </ul> 
+    </nav> 
+  ); 
+} 
+ 
 export default Navbar;
